7f11656d38079dcb59972ec45bca5bdd2254fd43
mpludowski <mpludowski@users.noreply.github.com>
2020-02-25 13:18:01 +0100

    docker ps

--- config.fish
+++ config.fish
@@ -7,3 +7,10 @@ set -g theme_newline_cursor yes
 function dr
     docker run --rm -it -v (pwd):/app -w /app $argv
 end
+
+function dps
+    for i in (docker ps --format '{{json .}}')
+        echo $i | json2yaml
+        echo ''
+    end | ccat
+end
