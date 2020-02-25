575aa6bbc3945594f013cf38eff31371b3349c0a
mpludowski <mpludowski@users.noreply.github.com>
2020-02-25 23:12:39 +0100

    docker ps updated and go in path

--- config.fish
+++ config.fish
@@ -3,13 +3,15 @@ alias dco="docker-compose"
 
 set -x MINIKUBE_IN_STYLE true
 set -g theme_newline_cursor yes
+set PATH ~/go/bin $PATH
 
 function dr
     docker run --rm -it -v (pwd):/app -w /app $argv
 end
 
 function dps
-    for i in (docker ps --format '{{json .}}')
+    for i in (docker ps --no-trunc --format '{{json .}}')
+        set i (echo $i | jq "{Names: .Names,ID: .ID,Image: .Image,Ports: .Ports,Command: .Command,CreatedAt: .CreatedAt,Labels: .Labels,LocalVolumes: .LocalVolumes,Mounts: .Mounts,Networks: .Networks,RunningFor: .RunningFor,Size: .Size,Status: .Status}")
         echo $i | json2yaml
         echo ''
     end | ccat
