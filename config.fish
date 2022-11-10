2f8da8a6c15bcb8359aca860da7ed18370dfe070
Michał Płudowski <michal.pludowski@codema.pl>
2022-11-10 13:43:39 +0100

    .

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 alias please="sudo"
-alias dco="docker-compose"
+alias dco="docker compose"
+alias dff="kitty +kitten diff"
 
 set -x MINIKUBE_IN_STYLE true
 set -g theme_newline_cursor yes
@@ -11,7 +12,7 @@ end
 
 function dps
     for i in (docker ps --no-trunc --format '{{json .}}')
-        set i (echo $i | jq "{Names: .Names,ID: .ID,Image: .Image,Ports: .Ports,Command: .Command,CreatedAt: .CreatedAt,Labels: .Labels,LocalVolumes: .LocalVolumes,Mounts: .Mounts,Networks: .Networks,RunningFor: .RunningFor,Size: .Size,Status: .Status}")
+        set i (echo $i | jq "{Names: .Names,ID: .ID,Image: .Image,Ports: .Ports,Command: .Command,CreatedAt: .CreatedAt,LocalVolumes: .LocalVolumes,Mounts: .Mounts,Networks: .Networks,RunningFor: .RunningFor,Size: .Size,Status: .Status}")
         echo $i | json2yaml
         echo ''
     end | ccat
